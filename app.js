21e376dbb0b6e4b8d2618fcc7e6b0d07eda1dd58
o6098 <oooo6098@gmail.com>
2016-04-04 11:18:09 +0800

    second

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var routers = require('./routers');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use('/assets', express(__dirname + '/public'));
+app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use('/', routers);
